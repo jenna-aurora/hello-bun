4f5d69abb41998b674d13aea10779422a3be7b83
jenna-aurora <jenna.a.cardenas@outlook.com>
2026-07-30 15:51:51 -0500

    start hello-bun with hello script and setup notes

---
+++ hello.js
@@ -0,0 +1,10 @@
+let course = "Level 2";
+let message = "Hello from " + course + "!";
+console.log(message);
+
+let name = " Jenna";
+let message1 = "My name is" + name + "!";
+console.log(message1);
+
+console.log("This JavaScript runs on MY machine.");
+
---
+++ notes.md
@@ -0,0 +1,5 @@
+# My Setup
+- OS: (Windows + version)
+- Shell: (Git Bash)
+- Bun version: (from bun --1.3.14)
+- Editor: (Notepad++)
